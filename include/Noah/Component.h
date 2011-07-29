16bcb9eb0fdb44ce5c758fb5f3209394a0bfda0c
Ryan Jacobson <rjacobson@bravenet.com>
2011-07-29 13:18:59 -0700

    Added documentation

--- include/Noah/Component.h
+++ include/Noah/Component.h
@@ -1,5 +1,16 @@
+////////////////////////////////////////////////////////////
+//
+// Noah - A Component Based Entity System and Game Engine
+// Ryan Jacobson (rejacobson@gmail.com)
+//
+////////////////////////////////////////////////////////////
+
 #ifndef _COMPONENT_H
 #define _COMPONENT_H
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
 
 #include <Noah/EntitySystem.h>
 #include <iostream>
@@ -8,37 +19,81 @@
 namespace noah
 {
 
-// Component System Base
-//   This is used to keep heterogeneous lists of Component Systems
+////////////////////////////////////////////////////////////
+/// \brief Base class allows for heterogeneous lists of component systems
+///
+////////////////////////////////////////////////////////////
 class ComponentSystemBase
 {
-  public:
-    virtual void RegisterComponent( Entity*, ComponentBase* ) { }
-    virtual void KillComponent( EntityId ) { }
+  public:    
+    ////////////////////////////////////////////////////////////
+    /// \brief Add a component to this component system
+    ///
+    /// \param entity A pointer to the entity to add a component for
+    /// \param component A pointer to the component being added
+    ///
+    ////////////////////////////////////////////////////////////
+    virtual void RegisterComponent( Entity *entity, ComponentBase *component ) { }
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief Remove a component from the system
+    ///
+    /// \param entity_id The id of the entity to remove the component from
+    ///
+    ////////////////////////////////////////////////////////////
+    virtual void KillComponent( EntityId entity_id ) { }
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Update the system
+    ///
+    /// Typically this is used to update the system once per frame
+    ///
+    /// \param state A pointer to the current GameState
+    ///
+    ////////////////////////////////////////////////////////////
     virtual void Update( GameState *state = 0 ) { }
-    virtual void Initialize( EntityId, GameState* ) { }
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Run initialization code for an entity.
+    ///
+    /// This is intended to be run once for an entity after it's
+    /// components have been set.
+    ///
+    /// \param entity_id The id of the entity to initialize
+    /// \param state A pointer to the current GameState
+    ///
+    ////////////////////////////////////////////////////////////
+    virtual void Initialize( EntityId entity_id, GameState *state ) { }
 };
 
-
-
-// Component System
-//   This is the class that custom Component Systems should derive from.
+////////////////////////////////////////////////////////////
+/// \brief Component system manages a list of components of one type
+///
+/// This is the class that custom Component Systems should derive from.
+///
+////////////////////////////////////////////////////////////
 template <class TComponent>
 class ComponentSystem : public ComponentSystemBase
 {
   public:
-    // The list of components in this system.
-    std::map<EntityId, noah::safe_ptr<TComponent>> components_;
-
-    EntitySystem *entity_system_;
-
-    // Add a new component for an entity in the system.
-    void RegisterComponent( Entity *e, ComponentBase *c )
+    ////////////////////////////////////////////////////////////
+    /// \brief Add a new component to an entity
+    ///
+    /// \param entity A pointer to the entity
+    /// \param component A pointer to the component to add
+    ///
+    ////////////////////////////////////////////////////////////
+    void RegisterComponent( Entity *entity, ComponentBase *component )
     {
-      components_.insert( std::pair<EntityId, safe_ptr<TComponent>>( e->id_, safe_ptr<TComponent>((TComponent*)c) ) );
+      components_.insert( std::pair<EntityId, safe_ptr<TComponent>>( entity->id_, safe_ptr<TComponent>((TComponent*)component) ) );
     }
 
-    // Loop through each component and run a functor for it.
+    ////////////////////////////////////////////////////////////
+    /// \brief Run a functor on each component
+    ///
+    /// \param function Pointer to a functor that processes the component
+    ///
+    ////////////////////////////////////////////////////////////
     template <typename TFunctor>
     void EachComponent( TFunctor *functor )
     {
@@ -49,49 +104,104 @@ class ComponentSystem : public ComponentSystemBase
       }
     }
 
-    void KillComponent( EntityId eid )
+    ////////////////////////////////////////////////////////////
+    /// \brief Remove a component from an entity
+    ///
+    /// \param entity_id The id of the entity to remove the component for
+    ///
+    ////////////////////////////////////////////////////////////
+    void KillComponent( EntityId entity_id )
     {
-      std::map<EntityId, safe_ptr<TComponent>>::iterator toKill = components_.find(eid);
+      std::map<EntityId, safe_ptr<TComponent>>::iterator toKill = components_.find( entity_id );
+
+      // Delete the component held by the safe_ptr
       toKill->second.clear();
-      components_.erase(toKill);
-    }
 
-    safe_ptr<TComponent> GetComponent( EntityId eid )
+      // Remove the component from the master list
+      components_.erase( toKill );
+    }
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief Get a component by entity_id
+    ///
+    /// \param entity_id The id of the entity to get a component from
+    ///
+    /// \return Returns a safe_ptr to the component
+    ///
+    ////////////////////////////////////////////////////////////
+    safe_ptr<TComponent> GetComponent( EntityId entity_id )
     { 
-      return components_[eid];
+      return components_[ entity_id ];
     }
-
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the number of components in the system
+    ///
+    /// \return Returns the number of components in the system
+    ///
+    ////////////////////////////////////////////////////////////
     int Size( void )
     {
       return components_.size();
     }
+    
+    ////////////////////////////////////////////////////////////
+    // Member data
+    ////////////////////////////////////////////////////////////
+    std::map<EntityId, noah::safe_ptr<TComponent>> components_; //< The list of components in this system
+    EntitySystem *entity_system_;                               //< The entity system managing this component systme
 };
 
 
 
-//
-// Component System Base
-//
+////////////////////////////////////////////////////////////
+/// \brief Base component class
+///
+////////////////////////////////////////////////////////////
 class ComponentBase { };
 
-//
-// Component
-//
+////////////////////////////////////////////////////////////
+/// \brief The basic component object, managed by a component system
+///
+////////////////////////////////////////////////////////////
 template <typename TSystem>
 class Component : public ComponentBase
 {
   public:
-
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the family id (component system id) that this component belongs to
+    ///
+    /// \return Returns the FamilyId of the component system
+    ///
+    ////////////////////////////////////////////////////////////
     static FamilyId GetFamilyId( void )
     {
       return TSystem::family_id_;
     }
 
-    static TSystem *Cast( ComponentSystemBase *s )
+    ////////////////////////////////////////////////////////////
+    /// \brief Cast a ComponentSystemBase into this component's component system 
+    ///
+    /// \param base_system A pointer to a ComponentSystemBase object
+    ///
+    /// \return Returns the base object cast into the derived object
+    ///
+    ////////////////////////////////////////////////////////////
+    static TSystem *Cast( ComponentSystemBase *base_system )
     {
-      return (TSystem*)s;
+      return (TSystem*)base_system;
     }
 
+    ////////////////////////////////////////////////////////////
+    /// \brief Less than operator 
+    ///
+    /// Used for sorting algorithms
+    ///
+    /// \param Right hand side component
+    ///
+    /// \return Returns true if the lhs family_id is less than the rhs family_id
+    ///
+    ////////////////////////////////////////////////////////////
     bool operator<(Component rhs) { return GetFamilyId() < rhs.GetFamilyId(); }
 };
 
